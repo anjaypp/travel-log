aa096594d4c5b8027e1e812757919e44b5c70606
Anjay P.P <anjaypp20@gmail.com>
2025-04-16 19:57:56 +0530

    feat(models): add TravelLog and User models

---
+++ server/src/models/TravelLog.model.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const travelLogSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true,
+      maxlength: 1000
+    },
+    visitedDate: {
+      type: Date,
+      required: true
+    },
+    location: {
+      lat: {
+        type: Number,
+        required: true,
+        min: -90,
+        max: 90
+      },
+      lng: {
+        type: Number,
+        required: true,
+        min: -180,
+        max: 180
+      }
+    },
+    images: [
+        {
+          url: {
+            type: String,
+          },
+          publicId: {  
+            type: String,
+          },
+        },
+      ],
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      default: 0
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("TravelLog", travelLogSchema);
