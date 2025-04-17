91d3bf84baf589c728f2feee8a3d3655bda54583
Anjay P.P <anjaypp20@gmail.com>
2025-04-17 11:31:27 +0530

    feat: setup cloudinary configuration for image upload using environment variables

---
+++ server/src/lib/config/cloudinary.js
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from 'cloudinary';
+import config from './config.js';
+
+cloudinary.config({
+    cloud_name: config.cloudinary.CLOUD_NAME,
+    api_key: config.cloudinary.API_KEY,
+    api_secret: config.cloudinary.API_SECRET});
+
+export default cloudinary;
--- server/src/lib/config/config.js
+++ server/src/lib/config/config.js
@@ -5,7 +5,12 @@ dotenv.config({ path: './.env' });
 const config = {
   PORT: process.env.PORT || 3001,
   MONGODB_URI: process.env.MONGODB_URI,
-  NODE_ENV: process.env.NODE_ENV
+  NODE_ENV: process.env.NODE_ENV,
+  cloudinary: {
+    CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    API_KEY: process.env.CLOUDINARY_API_KEY,
+    API_SECRET: process.env.CLOUDINARY_API_SECRET
+  }
 };
 
 export default config;
