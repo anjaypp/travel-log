b740246e470ded1a8853a43460b0c6aa4b102cc7
Anjay P.P <anjaypp20@gmail.com>
2025-04-20 15:40:08 +0530

    feat: added marker and popup to the map.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
-import * as React from 'react';
-import Map from 'react-map-gl/mapbox';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import * as React from "react";
+import Map, { Marker, Popup } from "react-map-gl/mapbox";
+import { IoMdPin } from "react-icons/io";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 function App() {
   return (
@@ -11,10 +12,26 @@ function App() {
         latitude: 0,
         zoom: 1.5
       }}
-      style={{ width: '100vw', height: '100vh' }}
+      style={{ width: "100vw", height: "100vh" }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
+    >
+      <Marker longitude={-100} latitude={40} anchor="bottom">
+      <IoMdPin size={30} color="rgb(227, 43, 43)"/>
+      </Marker>
+
+      <Popup longitude={-100} latitude={40}
+        closeButton={true}
+        closeOnClick={false}
+        anchor="left"
+        style={{ color: "rgb(0, 0, 0)" }}>
+        <div>
+          <h1>Some Title</h1>
+          <p>Some description</p>
+        </div>
+      </Popup>
+
+    </Map>
   );
 }
 
-export default App
+export default App;
