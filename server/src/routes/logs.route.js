156ff7aedd3ba7a793eb2864cda426c134a89de9
Anjay P.P <anjaypp20@gmail.com>
2025-05-06 20:19:57 +0530

    refactor: added file upload middleware to log route.

--- server/src/routes/logs.route.js
+++ server/src/routes/logs.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { createTravelLog, getAllTravelLogs, getTravelLogById, updateTravelLog, deleteTravelLog } from "../controllers/logs.controller.js";
 import passport from "../lib/config/passport/index.js";
+import { handleFileUpload } from "../middlewares/fileUpload.js";
 const router = express.Router();
 
-router.post('/', passport.authenticate('jwt', { session: false }), createTravelLog);
+router.post('/', handleFileUpload,passport.authenticate('jwt', { session: false }), createTravelLog);
 router.get('/', passport.authenticate('jwt', { session: false }), getAllTravelLogs);
 router.get('/:id', passport.authenticate('jwt', { session: false }), getTravelLogById);
 router.put('/:id', passport.authenticate('jwt', { session: false }), updateTravelLog);
